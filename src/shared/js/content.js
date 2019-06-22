c277353624b85829b676592c735f35010f781542
Steven Christensen <stevenlevi.christensen@gmail.com>
2019-06-22 01:32:40 -0700

    Modifications to Selectors adding msqc as prefix for contentful entries in CSS and replaced references in .js files. modified README.md to list changes made to extension and why. Reworked content.js to accept Space ID as an additional paramater so when we have pages with mixed spaces  (think global header on landing page or homepage) we still get correct links per entry

--- src/shared/js/content.js
+++ src/shared/js/content.js
@@ -10,27 +10,31 @@ $(() => {
 
   getState.then((active) => {
     if (active) {
-      const CONTENTFUL_CURRENT_SPACE_ID = $('[name="contentful_space"]').attr('content');
-      const CONTENTFUL_ENVIRONMENT = $('[name="contentful_environment"]').attr('content');
-
-      const getContentfulUrl = (elementId) => {
-        return `https://app.contentful.com/spaces/${CONTENTFUL_CURRENT_SPACE_ID}/environments/${CONTENTFUL_ENVIRONMENT}/entries/${elementId}`;
+      const getContentfulUrl = (elementId, spaceId) => {
+        return `https://app.contentful.com/spaces/${spaceId}/entries/${elementId}`;
       };
 
-      if (CONTENTFUL_CURRENT_SPACE_ID && CONTENTFUL_ENVIRONMENT) {
-        $.each($('[data-csk-entry-id]'), (index, el) => {
-          // Get the data for each element
-          const elementId = $(el).data('csk-entry-id');
-          $(el)
-            .addClass('csk-element')
-            .off('click')
-            .on('click', (e) => {
-              if ($(e.target).data('csk-entry-id') !== elementId) return;
-              e.stopPropagation();
-              window.open(getContentfulUrl(elementId));
-            });
-        });
-      }
+
+      $.each($('[data-msqc-entry-id]'), (index, el) => {
+        // Get the data for each element
+        const elementId = $(el)
+          .data('msqc-entry-id');
+        const spaceId = $(el)
+          .data('msqc-space-id');
+        $(el)
+          .addClass('msqc-element')
+          .off('click')
+          .on('click', (e) => {
+            if ($(e.target)
+              .data('msqc-entry-id') !== elementId && $(e.target)
+              .data('msqc-space-id') !== spaceId) {
+              return;
+            }
+            e.stopPropagation();
+            window.open(getContentfulUrl(elementId, spaceId));
+          });
+      });
     }
+
   });
 });
